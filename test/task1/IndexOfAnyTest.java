f4d7e64c28951203077c0623a204e688b07c3b64
Dmitry Tishchenko <flir3d@gmail.com>
2015-11-10 10:08:36 +0300

    Task 1

--- test/task1/IndexOfAnyTest.java
+++ test/task1/IndexOfAnyTest.java
@@ -1,5 +1,36 @@
 package task1;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class IndexOfAnyTest {
-  //Your tests go here
+	@Test
+	public void testSearchTheOnly() {
+		int resultIndex = IndexOfAny.indexOfAny("abcedf", new char[] {'c'});
+		Assert.assertEquals(2, resultIndex);
+	}
+	
+	@Test
+	public void testSearchMultiple() {
+		int resultIndex = IndexOfAny.indexOfAny("abcedf", new char[] {'b','c', 'a'});
+		Assert.assertEquals(0, resultIndex);
+	}
+	
+	@Test
+	public void testSearchNone() {
+		int resultIndex = IndexOfAny.indexOfAny("abcedf", new char[] {});
+		Assert.assertEquals(-1, resultIndex);
+	}
+	
+	@Test
+	public void testSearchNoneExistent() {
+		int resultIndex = IndexOfAny.indexOfAny("abcedf", new char[] {'g'});
+		Assert.assertEquals(-1, resultIndex);
+	}
+	
+	@Test
+	public void testSearchEmptyString() {
+		int resultIndex = IndexOfAny.indexOfAny("", new char[] {'a'});
+		Assert.assertEquals(-1, resultIndex);
+	}
 }
